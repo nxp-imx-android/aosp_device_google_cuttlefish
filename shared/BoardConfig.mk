baaf4c6050757f28adaadb263ab4e2f7221ab11e
Haoran Wang <elven.wang@nxp.com>
2024-07-30 16:48:58 +0800

    MA-22692-3 Reduce the boot logs
    Change-Id: I1975bb8dec6edf57f7e8abdd35c7b80177bcc904
    Signed-off-by: Haoran Wang <elven.wang@nxp.com>

--- shared/BoardConfig.mk
+++ shared/BoardConfig.mk
@@ -316,10 +316,10 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 # To see full logs from init, disable ratelimiting.
 # The default is 5 messages per second amortized, with a burst of up to 10.
-BOARD_KERNEL_CMDLINE += printk.devkmsg=on
+#BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 
 # Print audit messages for all security check failures
-BOARD_KERNEL_CMDLINE += audit=1
+#BOARD_KERNEL_CMDLINE += audit=1
 
 # Reboot immediately on panic
 BOARD_KERNEL_CMDLINE += panic=-1
